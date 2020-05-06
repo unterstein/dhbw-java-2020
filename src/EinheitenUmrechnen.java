5704e56c5ce6ae7e932185479d62be18fc5b95bf
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-05-06 21:07:07 +0200

    Aufgaben

---
+++ src/EinheitenUmrechnen.java
@@ -0,0 +1,15 @@
+public class EinheitenUmrechnen {
+    // Celsius = 5/9 * (Fahrenheit - 32).
+    // 9/5 * Celsius + 32 = Fahrenheit
+    // Celsius = Kelvin - 273.15.
+    // Die tiefste mögliche Temperatur ist den absoluten Nullpunkt 0K.
+    public static void main(String[] args) {
+       double temperaturInCelsion = 40.2;
+       // Aufgabe hier: Ausgabe in Kelvin und Fahrenheit
+
+        // Celsius -> Fahrenheit
+        double temperaturInFahrenheit = 105;
+        // Fahreneinheit -> Celsius ODER Fahrenheit -> Kelvin
+    }
+    // Verwende Funktionen zur Berechnung!
+}
