6b41b99eca1336ef45f0457ed3ba707613a78349
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-05-21 22:03:46 +0200

    Beispiel

---
+++ src/kapitel3/HelloWorld.java
@@ -0,0 +1,11 @@
+package kapitel3;
+
+public class HelloWorld {
+    static char c;
+    static String s;
+
+    public static void main(String[] args) {
+        System.out.println("->" + c + "<-");
+        System.out.println(s);
+    }
+}
