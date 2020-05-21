fe9ad27e46b92f23bb441fb6bac949d0cbfc90c7
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-05-21 23:05:17 +0200

    Kapitel 3 Video 5

--- src/kapitel3/LiveCoding.java
+++ src/kapitel3/LiveCoding.java
@@ -34,5 +34,36 @@ public class LiveCoding {
             AudiQ5 dritterQ5 = (AudiQ5) pkw2;
             System.out.println(dritterQ5);
         }
+
+        int test = 4;
+        String ausgabe = (test == 4) ? "Hallo!" : "Welt!";
+        System.out.println(ausgabe);
+
+        String ausgabe2;
+        if (test == 4) {
+            ausgabe2 = "Hallo!";
+        } else {
+            ausgabe2 = "Welt!";
+        }
+        System.out.println(ausgabe2);
+
+        System.out.println(ausgabe + ausgabe2);
+        String neuerString = ausgabe + ausgabe2;
+        String nochEinNeuerString = pkw2 + ausgabe + i2;
+        System.out.println(nochEinNeuerString);
+
+        System.out.println(1 == 1);
+        String string1 = new String("Hallo"); // String string1 = "Hallo";
+        String string2 = new String("Hallo"); // String string2 = "Hallo";
+        System.out.println(string1 == string2);
+        System.out.println(string1.equals(string2));
+
+        System.out.println(pkw2 instanceof Pkw);
+        System.out.println(pkw2 instanceof SUV);
+        System.out.println(pkw2 instanceof AudiQ7);
+        System.out.println(pkw2 instanceof AudiQ5);
+
+        pkw2.anzahlBlinker = 4;
+        pkw2.blink();
     }
 }
--- src/kapitel3/model/Pkw.java
+++ src/kapitel3/model/Pkw.java
@@ -1,4 +1,9 @@
 package kapitel3.model;
 
 public class Pkw {
+    public int anzahlBlinker;
+
+    public void blink() {
+
+    }
 }
