192dba409d1c40fd918e2529667bb4dcc3438f09
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-05-21 20:45:25 +0200

    Klassen verschoben

--- src/Sortierung.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Sortierung {
-    public static void main(String[] args) {
-        // Eingabe:
-        int[] zuSortieren = {12, 14, 125, 1, 9, 28, 3};
-        // Ausgabe: Sortierte Liste
-    }
-}
