6474596f8bfcaff908f919408288f0e60168209f
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-05-23 01:08:36 +0200

    Kapitel 4 Video 5

---
+++ src/kapitel4/Sichtbarkeit2.java
@@ -0,0 +1,22 @@
+package kapitel4;
+
+
+public class Sichtbarkeit2 extends Sichtbarkeit {
+    private int i = 7;
+
+    public Sichtbarkeit2() {
+        int i = 4;
+        printInt();
+        printInt(this.i + i);
+    }
+
+    @Override
+    protected void printInt(int i) {
+        System.out.println(i + 5);
+    }
+
+    public static void main(String[] args) {
+        new Sichtbarkeit2();
+    }
+}
+
